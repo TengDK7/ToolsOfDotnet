3c64e297695f34c62e61616888622c323a21d821
TengDK7 <2733934409@qq.com>
2021-12-19 12:53:37 +0800

    log4net init

---
+++ ToolOfLog4net/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolOfLog4net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LogHelper.WriteLog(string.Format("当前时间为{0}.", DateTime.Now.ToString()));
+            LogHelper.WriteLog("Cautch Exception:", new Exception("My exception!"));
+            System.Threading.Thread.Sleep(5000);
+            LogHelper.WriteLog(string.Format("当前时间为{0}.", DateTime.Now.ToString()));
+            Console.ReadKey();
+        }
+    }
+}
